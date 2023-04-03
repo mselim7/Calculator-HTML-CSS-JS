ec8a5453b7580c2de2e2451b44f13f59a444becf
Mahmoud Selim <73776713+mselim7@users.noreply.github.com>
2023-04-03 22:27:15 +0200

    Better JS

--- script.js
+++ script.js
@@ -1,73 +1,102 @@
 'use strict';
-let size = document.querySelector('.problem').value;
-document.getElementById('9').addEventListener('click', function () {
-  document.querySelector('.problem').value += '9';
-  size = document.querySelector('.problem').value;
+
+const num9 = document.getElementById('9');
+const num8 = document.getElementById('8');
+const num7 = document.getElementById('7');
+const num6 = document.getElementById('6');
+const num5 = document.getElementById('5');
+const num4 = document.getElementById('4');
+const num3 = document.getElementById('3');
+const num2 = document.getElementById('2');
+const num1 = document.getElementById('1');
+const num0 = document.getElementById('0');
+
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const mult = document.getElementById('mult');
+const divd = document.getElementById('divd');
+const prop = document.querySelector('.problem');
+let size = prop.value;
+
+num9.addEventListener('click', function () {
+  prop.value += '9';
+  size = prop.value;
 });
-document.getElementById('8').addEventListener('click', function () {
-  document.querySelector('.problem').value += '8';
-  size = document.querySelector('.problem').value;
+num8.addEventListener('click', function () {
+  prop.value += '8';
+  size = prop.value;
 });
-document.getElementById('7').addEventListener('click', function () {
-  document.querySelector('.problem').value += '7';
-  size = document.querySelector('.problem').value;
+num7.addEventListener('click', function () {
+  prop.value += '7';
+  size = prop.value;
 });
-document.getElementById('6').addEventListener('click', function () {
-  document.querySelector('.problem').value += '6';
-  size = document.querySelector('.problem').value;
+num6.addEventListener('click', function () {
+  prop.value += '6';
+  size = prop.value;
 });
-document.getElementById('5').addEventListener('click', function () {
-  document.querySelector('.problem').value += '5';
-  size = document.querySelector('.problem').value;
+num5.addEventListener('click', function () {
+  prop.value += '5';
+  size = prop.value;
 });
-document.getElementById('4').addEventListener('click', function () {
-  document.querySelector('.problem').value += '4';
-  size = document.querySelector('.problem').value;
+num4.addEventListener('click', function () {
+  prop.value += '4';
+  size = prop.value;
 });
-document.getElementById('3').addEventListener('click', function () {
-  document.querySelector('.problem').value += '3';
-  size = document.querySelector('.problem').value;
+num3.addEventListener('click', function () {
+  prop.value += '3';
+  size = prop.value;
 });
-document.getElementById('2').addEventListener('click', function () {
-  document.querySelector('.problem').value += '2';
-  size = document.querySelector('.problem').value;
+num2.addEventListener('click', function () {
+  prop.value += '2';
+  size = prop.value;
 });
-document.getElementById('1').addEventListener('click', function () {
-  document.querySelector('.problem').value += '1';
-  size = document.querySelector('.problem').value;
+num1.addEventListener('click', function () {
+  prop.value += '1';
+  size = prop.value;
 });
-document.getElementById('0').addEventListener('click', function () {
-  document.querySelector('.problem').value += '0';
-  size = document.querySelector('.problem').value;
+num0.addEventListener('click', function () {
+  prop.value += '0';
+  size = prop.value;
 });
-document.getElementById('plus').addEventListener('click', function () {
-  document.querySelector('.problem').value += '+';
-  size = document.querySelector('.problem').value;
+plus.addEventListener('click', function () {
+  prop.value += '+';
+  size = prop.value;
 });
-document.getElementById('minus').addEventListener('click', function () {
-  document.querySelector('.problem').value += '-';
-  size = document.querySelector('.problem').value;
+minus.addEventListener('click', function () {
+  prop.value += '-';
+  size = prop.value;
 });
-document.getElementById('mult').addEventListener('click', function () {
-  document.querySelector('.problem').value += '×';
-  size = document.querySelector('.problem').value;
+mult.addEventListener('click', function () {
+  prop.value += '×';
+  size = prop.value;
 });
-document.getElementById('divd').addEventListener('click', function () {
-  document.querySelector('.problem').value += '÷';
-  size = document.querySelector('.problem').value;
+divd.addEventListener('click', function () {
+  prop.value += '÷';
+  size = prop.value;
 });
 
 document.getElementById('dot').addEventListener('click', function () {
-  document.querySelector('.problem').value += '.';
-  size = document.querySelector('.problem').value;
+  prop.value += '.';
+  size = prop.value;
 });
-console.log(size);
+
+if (size.length >= 15) {
+  let buttons = document.getElementsByTagName('button');
+  for (let i = 0; i < buttons.length - 1; i++) {
+    if (buttons[i].id !== 'equal') {
+      buttons[i].disabled = true;
+    }
+  }
+  size = size.slice(0, 15);
+}
+
 document.getElementById('c').addEventListener('click', function () {
-  document.querySelector('.problem').value = '';
+  prop.value = '';
 });
 document.getElementById('equal').addEventListener('click', function () {
-  size = document.querySelector('.problem').value;
+  size = prop.value;
 
+  console.log(size);
   let n1 = '',
     n2 = '',
     res = '';
@@ -95,5 +124,5 @@ document.getElementById('equal').addEventListener('click', function () {
       }
     }
   }
-  document.querySelector('.problem').value = res;
+  prop.value = res;
 });
